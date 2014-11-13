8e7c70e2551020a3ade541758c08a38aace1ce04
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2014-11-13 08:16:00 -0600

    main.rkt stubs for sessions 2 and 3

---
+++ msb/session-002/main.rkt
@@ -0,0 +1,4 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname main) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+;; Session #2 – HtDP2e: Part 1, Sections 1 thru 2.4
