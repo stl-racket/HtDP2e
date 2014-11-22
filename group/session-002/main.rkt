6ecba6e94d7554c4bba25929c3c5ed33932a105a
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2014-11-22 16:07:32 -0600

    main.rkt for group's session-002

---
+++ group/session-002/main.rkt
@@ -0,0 +1,7 @@
+;; Session #2 – HtDP2e: Part 1, Sections 1 thru 2.4
+;; http://www.ccs.neu.edu/home/matthias/HtDP2e/
+
+;; 01 Nov 2014, LockerDome conference room
+;; vnc server:  192.168.88.126, password:  racket
+
+;; Code written collaboratively during the session was accidentally not saved.
