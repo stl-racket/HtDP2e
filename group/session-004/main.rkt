cf0b91005b705eb23d90487bdca52cf112d6aaac
STL Racket Laptop <stl-racket-laptop@stljs.org>
2014-12-06 01:30:20 -0600

    stub for group session #4

---
+++ group/session-004/main.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname main) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+;; Session #4 – HtDP2e: Part 1, Sections 4 thru 4.7
+;; http://www.ccs.neu.edu/home/matthias/HtDP2e/
+
+;; 06 Dec 2014, LockerDome conference room
+;; vnc server:  192.168.88.126, password:  racket
